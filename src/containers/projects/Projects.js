490129b121cb2607ec0c385da62a50263743ddbb
TimCheng <timazoey@gmail.com>
2019-11-18 18:44:00 +0800

    update#1

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -6,29 +6,29 @@ import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard";
 import Button from "../../components/button/Button";
 
 export default function Projects() {
-  const [repo, setrepo] = useState([]);
+    const [repo, setrepo] = useState([]);
 
-  useEffect(() => {
-    getRepoData();
-  }, []);
+    useEffect(() => {
+        getRepoData();
+    }, []);
 
-  function getRepoData() {
-    const client = new ApolloClient({
-      uri: "https://api.github.com/graphql",
-      request: operation => {
-        operation.setContext({
-          headers: {
-            authorization: `Bearer ${atob("YOUR CONVERTED GITHUB TOKEN")}`
-          }
+    function getRepoData() {
+        const client = new ApolloClient({
+            uri: "https://api.github.com/graphql",
+            request: operation => {
+                operation.setContext({
+                    headers: {
+                        authorization: `Bearer ${atob("17de5ce772b13904c1c8180398fc7083d9195a93")}`
+                    }
+                });
+            }
         });
-      }
-    });
 
-    client
-      .query({
-        query: gql`
+        client
+            .query({
+                query: gql `
           {
-            repositoryOwner(login: "your github username") {
+            repositoryOwner(login: "Tim810306") {
               ... on User {
                 pinnedRepositories(first: 6) {
                   edges {
@@ -53,26 +53,35 @@ export default function Projects() {
             }
           }
         `
-      })
-      .then(result => {
-        setrepoFunction(result.data.repositoryOwner.pinnedRepositories.edges);
-        console.log(result);
-      });
-  }
+            })
+            .then(result => {
+                setrepoFunction(result.data.repositoryOwner.pinnedRepositories.edges);
+                console.log(result);
+            });
+    }
 
-  function setrepoFunction(array) {
-    setrepo(array);
-  }
+    function setrepoFunction(array) {
+        setrepo(array);
+    }
 
-  return (
-    <div className="main" id="opensource">
-      <h1 className="project-title">Open Source Projects</h1>
-      <div className="repo-cards-div-main">
-        {repo.map((v, i) => {
-          return <GithubRepoCard repo={v} key={v.node.id} />;
-        })}
-      </div>
-      <Button text={"More Projects"} className="project-button" href="https://github.com/saadpasta" newTab={true} />
-    </div>
-  );
+    return ( <
+        div className = "main"
+        id = "opensource" >
+        <
+        h1 className = "project-title" > Open Source Projects < /h1> <
+        div className = "repo-cards-div-main" > {
+            repo.map((v, i) => {
+                return <GithubRepoCard repo = { v }
+                key = { v.node.id }
+                />;
+            })
+        } <
+        /div> <
+        Button text = { "More Projects" }
+        className = "project-button"
+        href = "https://github.com/saadpasta"
+        newTab = { true }
+        /> <
+        /div>
+    );
 }
